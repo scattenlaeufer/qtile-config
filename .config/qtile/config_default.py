e8f93734fdc682564a734d07087b1b80169efa87
Björn Guth <bjoern.guth@aixigo.com>
2024-10-23 10:00:14 +0200

    Current status after move to new laptop

--- .config/qtile/config_default.py
+++ .config/qtile/config_default.py
@@ -24,85 +24,36 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-import subprocess
-from pathlib import Path
-
-from libqtile import bar, hook, layout, qtile, widget
-from libqtile.backend.wayland import InputConfig
+from libqtile import bar, layout, qtile, widget
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
-from qtile_bonsai import Bonsai
 
 mod = "mod4"
 terminal = guess_terminal()
-
-lock_cmd = "swaylock --color 000000 --show-failed-attempts"
-rofi_cmd = "rofi -show drun"
-
-
-@hook.subscribe.startup_once
-def autostart():
-    subprocess.call(str(Path("~/.config/qtile/autostart.sh").expanduser()))
-    # lazy.spawn("kanshi")
-
-
-@hook.subscribe.suspend
-def suspend_lock():
-    qtile.spawn(lock_cmd)
-
-
-neo = {
-    "h": "s",
-    "j": "n",
-    "k": "k",
-    "l": "t",
-    "s": "i",
-    "n": "b",
-}
 
 keys = [
     # A list of available commands that can be bound to keys can be found
     # at https://docs.qtile.org/en/latest/manual/config/lazy.html
     # Switch between windows
-    Key([mod], neo["h"], lazy.layout.left(), desc="Move focus to left"),
-    Key([mod], neo["l"], lazy.layout.right(), desc="Move focus to right"),
-    Key([mod], neo["j"], lazy.layout.down(), desc="Move focus down"),
-    Key([mod], neo["k"], lazy.layout.up(), desc="Move focus up"),
+    Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
+    Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
+    Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
+    Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
     Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
     # Move windows between left/right columns or move up/down in current stack.
     # Moving out of range in Columns layout will create new column.
-    Key(
-        [mod, "shift"],
-        neo["h"],
-        lazy.layout.shuffle_left(),
-        desc="Move window to the left",
-    ),
-    Key(
-        [mod, "shift"],
-        neo["l"],
-        lazy.layout.shuffle_right(),
-        desc="Move window to the right",
-    ),
-    Key([mod, "shift"], neo["j"], lazy.layout.shuffle_down(), desc="Move window down"),
-    Key([mod, "shift"], neo["k"], lazy.layout.shuffle_up(), desc="Move window up"),
+    Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
+    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
+    Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
+    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
     # Grow windows. If current window is on the edge of screen and direction
     # will be to screen edge - window would shrink.
-    Key(
-        [mod, "control"],
-        neo["h"],
-        lazy.layout.grow_left(),
-        desc="Grow window to the left",
-    ),
-    Key(
-        [mod, "control"],
-        neo["l"],
-        lazy.layout.grow_right(),
-        desc="Grow window to the right",
-    ),
-    Key([mod, "control"], neo["j"], lazy.layout.grow_down(), desc="Grow window down"),
-    Key([mod, "control"], neo["k"], lazy.layout.grow_up(), desc="Grow window up"),
-    Key([mod], neo["n"], lazy.layout.normalize(), desc="Reset all window sizes"),
+    Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
+    Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
+    Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
+    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
+    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
     # Toggle between split and unsplit sides of stack.
     # Split = all windows displayed
     # Unsplit = 1 window displayed, like Max layout, but still with
@@ -116,30 +67,17 @@ keys = [
     Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
     # Toggle between different layouts as defined below
     Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
-    Key([mod], "x", lazy.window.kill(), desc="Kill focused window"),
+    Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
     Key(
         [mod],
-        "e",
+        "f",
         lazy.window.toggle_fullscreen(),
         desc="Toggle fullscreen on the focused window",
     ),
-    Key(
-        [mod, "shift"],
-        "space",
-        lazy.window.toggle_floating(),
-        desc="Toggle floating on the focused window",
-    ),
-    Key(
-        [mod, "control"],
-        "r",
-        lazy.reload_config(),
-        # lazy.spawn(Path("~/.config/polybar/startup.sh")),
-        # lazy.spawn("feh --bg-scale ~/.config/i3/green-galaxy.jpg"),
-        desc="Reload the config",
-    ),
+    Key([mod], "t", lazy.window.toggle_floating(), desc="Toggle floating on the focused window"),
+    Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-    Key([mod], "a", lazy.spawn(rofi_cmd), desc="Spawn rofi to launce a program"),
-    # Key([], "Pause", lazy.spawn(lock_cmd), desc="Lock screen on key press"),
+    Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
 ]
 
 # Add key bindings to switch VTs in Wayland.
@@ -183,7 +121,7 @@ for i in groups:
     )
 
 layouts = [
-    layout.Columns(border_focus_stack=["#0788c9", "#002376"], border_width=1),
+    layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
     layout.Max(),
     # Try more layouts by unleashing below layouts.
     # layout.Stack(num_stacks=2),
@@ -193,114 +131,55 @@ layouts = [
     # layout.MonadWide(),
     # layout.RatioTile(),
     # layout.Tile(),
-    layout.TreeTab(),
+    # layout.TreeTab(),
     # layout.VerticalTile(),
     # layout.Zoomy(),
-    Bonsai(
-        **{
-            "window.border_size": 1,
-            "tab_bar.height": 20,
-            # "tab_bar.tab.title_provider": lambda _, a, _: a.window.name,
-        }
-    ),
 ]
 
 widget_defaults = dict(
     font="sans",
-    fontsize=10,
+    fontsize=12,
     padding=3,
 )
 extension_defaults = widget_defaults.copy()
 
-clock_widget = widget.Clock(format="KW%W %Y-%m-%d %a %H:%M:%S")
-volume_widget = widget.Volume()
-
-bar_height = 20
-top_bar = bar.Bar(
-    [
-        widget.CurrentLayout(),
-        widget.GroupBox(),
-        widget.Prompt(),
-        widget.WindowName(),
-        widget.Chord(
-            chords_colors={
-                "launch": ("#ff0000", "#ffffff"),
-            },
-            name_transform=lambda name: name.upper(),
-        ),
-        widget.TextBox("default config", name="default"),
-        widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
-        # NB Systray is incompatible with Wayland, consider using StatusNotifier instead
-        widget.StatusNotifier(),
-        volume_widget,
-        clock_widget,
-        widget.QuickExit(),
-    ],
-    bar_height,
-    # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
-    # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
-)
-
-wallpaper_path = Path("~/.config/qtile/backgrounds/green-galaxy.jpg")
-
 screens = [
     Screen(
-        top=top_bar,
-        wallpaper=str(wallpaper_path.expanduser()),
-        wallpaper_mode="fill",
-    ),
-    Screen(
-        top=bar.Bar(
+        bottom=bar.Bar(
             [
                 widget.CurrentLayout(),
                 widget.GroupBox(),
+                widget.Prompt(),
                 widget.WindowName(),
-                clock_widget,
+                widget.Chord(
+                    chords_colors={
+                        "launch": ("#ff0000", "#ffffff"),
+                    },
+                    name_transform=lambda name: name.upper(),
+                ),
+                widget.TextBox("default config", name="default"),
+                widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
+                # NB Systray is incompatible with Wayland, consider using StatusNotifier instead
+                # widget.StatusNotifier(),
+                widget.Systray(),
+                widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
+                widget.QuickExit(),
             ],
-            bar_height,
+            24,
+            # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
+            # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
         ),
-        wallpaper=str(wallpaper_path.expanduser()),
-        wallpaper_mode="fill",
-    ),
-    Screen(
-        top=bar.Bar(
-            [
-                widget.CurrentLayout(),
-                widget.GroupBox(),
-                widget.WindowName(),
-                clock_widget,
-            ],
-            bar_height,
-        ),
-        wallpaper=str(wallpaper_path.expanduser()),
-        wallpaper_mode="fill",
-    ),
-    Screen(
-        top=bar.Bar(
-            [
-                widget.CurrentLayout(),
-                widget.GroupBox(),
-                widget.WindowName(),
-                clock_widget,
-            ],
-            bar_height,
-        ),
-        wallpaper=str(wallpaper_path.expanduser()),
-        wallpaper_mode="fill",
+        # You can uncomment this variable if you see that on X11 floating resize/moving is laggy
+        # By default we handle these events delayed to already improve performance, however your system might still be struggling
+        # This variable is set to None (no cap) by default, but you can set it to 60 to indicate that you limit it to 60 events per second
+        # x11_drag_polling_rate = 60,
     ),
 ]
 
 # Drag floating layouts.
 mouse = [
-    Drag(
-        [mod],
-        "Button1",
-        lazy.window.set_position_floating(),
-        start=lazy.window.get_position(),
-    ),
-    Drag(
-        [mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()
-    ),
+    Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
+    Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
     Click([mod], "Button2", lazy.window.bring_to_front()),
 ]
 
@@ -331,7 +210,7 @@ reconfigure_screens = True
 auto_minimize = True
 
 # When using the Wayland backend, this can be used to configure input devices.
-wl_input_rules = {"type:keyboard": InputConfig(kb_layout="de", kb_variant="neo")}
+wl_input_rules = None
 
 # xcursor theme (string or None) and size (integer) for Wayland backend
 wl_xcursor_theme = None
